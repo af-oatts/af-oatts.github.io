f65d2b87e8017c51b88314069d8ab1a8d3da7299
David Campbell <dcampbell@dcscorp.com>
2025-10-14 15:54:16 -0500

    Create Statically generated asset pages

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,10 @@
-export const GITHUB_OWNER = 'af-oatts'
-export const GITHUB_REPO = 'boatts'
+export const GITHUB_OWNER = "af-oatts";
+export const GITHUB_REPO = "boatts";
+
+export const BUILD_TARGETS = {
+  MAC_OS_ARM: "macos_arm_gz",
+  MAC_OS_INTEL: "macos_intel_gz",
+  LINUX: "linux",
+  WINDOWS_EXE: "windows_exe",
+  WINDOWS_MSI: "windows_msi",
+};
