23aa269c5c328b166b25ded781d6d09eb51891ee
David Campbell <dcampbell@dcscorp.com>
2025-10-15 09:37:55 -0500

    Make build targets constants.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,9 +2,9 @@ export const GITHUB_OWNER = "af-oatts";
 export const GITHUB_REPO = "boatts";
 
 export const BUILD_TARGETS = {
-  MAC_OS_ARM: "macos_arm_gz",
-  MAC_OS_INTEL: "macos_intel_gz",
-  LINUX: "linux",
+  MAC_OS_ARM: "macos_arm",
+  MAC_OS_INTEL: "macos_intel",
+  LINUX: "linux_appimage",
   WINDOWS_EXE: "windows_exe",
   WINDOWS_MSI: "windows_msi",
 };
