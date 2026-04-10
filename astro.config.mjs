b058d4471683698a4f76fdaa1b5cee42cd3a5eef
Alex H. <36968238+hooperdev@users.noreply.github.com>
2026-04-10 08:32:06 -0400

    Fixed partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
+
+
+
 
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  integrations: [],
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push", "gtag"],
+      },
+    }),
+  ],
 });
